0cf65ea22a2a02788faeaa08675fc693d7f5b018
Rafik El Hadi Houari <rehhouari@gmail.com>
2025-03-25 22:54:00 +0100

    Add support for alt. service name (fix #4)

--- prefs.js
+++ prefs.js
@@ -1,108 +1,141 @@
-import Adw from "gi://Adw"
-import Gio from "gi://Gio"
-import Gtk from "gi://Gtk"
+import Adw from 'gi://Adw'
+import Gio from 'gi://Gio'
+import Gtk from 'gi://Gtk'
 
-import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js'
+import {
+  ExtensionPreferences,
+  gettext as _,
+} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js'
 
 export default class SyncthingToggleExtensionPreferences extends ExtensionPreferences {
-	fillPreferencesWindow(window) {
-		window._settings = this.getSettings('org.gnome.shell.extensions.syncthing-toggle')
-		this._window = window
+  fillPreferencesWindow(window) {
+    window._settings = this.getSettings(
+      'org.gnome.shell.extensions.syncthing-toggle'
+    )
+    this._window = window
 
-		const iconTheme = Gtk.IconTheme.get_for_display(window.get_display())
-		const iconsDirectory = this.dir.get_child('icons').get_path()
-		iconTheme.add_search_path(iconsDirectory)
+    const iconTheme = Gtk.IconTheme.get_for_display(window.get_display())
+    const iconsDirectory = this.dir.get_child('icons').get_path()
+    iconTheme.add_search_path(iconsDirectory)
 
-		this._general()
-	}
-	_general() {
-		const page = new Adw.PreferencesPage()
-		this._window.add(page)
+    this._general()
+  }
+  _general() {
+    const page = new Adw.PreferencesPage()
+    this._window.add(page)
 
-		const group = new Adw.PreferencesGroup({
-			title: _('General Settings'),
-			description: _('Configure Syncthing Toggle'),
-		})
-		page.add(group)
+    const group = new Adw.PreferencesGroup({
+      title: _('General Settings'),
+      description: _('Configure Syncthing Toggle'),
+    })
+    page.add(group)
 
-		//#region port
+    //#region servicename
 
-		const portRow = new Adw.ActionRow({
-			title: "Syncthing port",
-			subtitle: "Set the port Syncthing runs on.",
-		})
+    const serviceNameRow = new Adw.EntryRow({
+      title: _('Service name'),
+    })
 
-		let port = this._window._settings.get_int('port')
+    group.add(serviceNameRow)
+    this._window._settings.bind(
+      'service-name',
+      serviceNameRow,
+      'text',
+      Gio.SettingsBindFlags.DEFAULT
+    )
 
-		const portInput = new Gtk.SpinButton({
-			valign: Gtk.Align.CENTER,
-			orientation: Gtk.Orientation.HORIZONTAL,
-			numeric: true,
-			adjustment: new Gtk.Adjustment({
-				lower: 0,
-				upper: 65535,
-				step_increment: 1,
-				value: port,
-			}),
-		})
+    //#endregion
 
-		portRow.add_suffix(portInput)
-		portRow.activatable_widget = portInput
-		group.add(portRow)
+    //#region port
 
-		this._window._settings.bind(
-			"port",
-			portInput,
-			"value",
-			Gio.SettingsBindFlags.DEFAULT
-		)
+    const portRow = new Adw.ActionRow({
+      title: 'Syncthing port',
+      subtitle: 'Set the port Syncthing runs on.',
+    })
 
-		//#endregion
+    let port = this._window._settings.get_int('port')
 
-		//#region stoponly
+    const portInput = new Gtk.SpinButton({
+      valign: Gtk.Align.CENTER,
+      orientation: Gtk.Orientation.HORIZONTAL,
+      numeric: true,
+      adjustment: new Gtk.Adjustment({
+        lower: 0,
+        upper: 65535,
+        step_increment: 1,
+        value: port,
+      }),
+    })
 
-		const startStopOnlySwitch = new Adw.SwitchRow({
-			title: _('Start/Stop only'),
-			subtitle: _('Whether or not to only start/stop or also enable/disable the Syncthing service when toggling.'),
-		})
+    portRow.add_suffix(portInput)
+    portRow.activatable_widget = portInput
+    group.add(portRow)
 
-		group.add(startStopOnlySwitch)
-		this._window._settings.bind('start-stop-only', startStopOnlySwitch, 'active',
-			Gio.SettingsBindFlags.DEFAULT)
+    this._window._settings.bind(
+      'port',
+      portInput,
+      'value',
+      Gio.SettingsBindFlags.DEFAULT
+    )
 
-		//#endregion
+    //#endregion
 
-		//#region about button
-		// About button - Thanks to @maniacx this could be done perfectly! 
-		const about_button = new Gtk.Button({
-			margin_top: 8,
-			margin_bottom: 8,
-			child: new Adw.ButtonContent({
-				icon_name: 'dialog-information-symbolic',
-				label: _('About')
-			})
-		})
+    //#region stoponly
 
-		// About Button Styles
-		about_button.set_css_classes(['accent'])
+    const startStopOnlySwitch = new Adw.SwitchRow({
+      title: _('Start/Stop only'),
+      subtitle: _(
+        'Whether or not to only start/stop or also enable/disable the Syncthing service when toggling.'
+      ),
+    })
 
-		group.set_header_suffix(about_button)
+    group.add(startStopOnlySwitch)
+    this._window._settings.bind(
+      'start-stop-only',
+      startStopOnlySwitch,
+      'active',
+      Gio.SettingsBindFlags.DEFAULT
+    )
 
-		about_button.connect('clicked', () => { this._about() })
+    //#endregion
 
-		//#endregion
-	}
+    //#region about button
 
-	_about() {
-		const about_window = new Adw.AboutWindow({ transient_for: this._window, modal: true })
-		about_window.set_application_icon('syncthing-logo-only')
-		about_window.set_application_name(_('Syncthing Toggle'))
-		about_window.set_version(`${this.metadata.version}.0`)
-		about_window.set_developer_name('rehhouari (rehhouari@gmail.com)')
-		about_window.set_issue_url(this.metadata.url + '/issues')
-		about_window.set_website(this.metadata.url)
-		about_window.set_license_type(Gtk.License.GPL_3_0)
-		about_window.set_copyright('© 2024 rehhouari')
-		about_window.show()
-	}
+    // About button - Thanks to @maniacx this could be done perfectly!
+    const about_button = new Gtk.Button({
+      margin_top: 8,
+      margin_bottom: 8,
+      child: new Adw.ButtonContent({
+        icon_name: 'dialog-information-symbolic',
+        label: _('About'),
+      }),
+    })
+
+    // About Button Styles
+    about_button.set_css_classes(['accent'])
+
+    group.set_header_suffix(about_button)
+
+    about_button.connect('clicked', () => {
+      this._about()
+    })
+
+    //#endregion
+  }
+
+  _about() {
+    const about_window = new Adw.AboutWindow({
+      transient_for: this._window,
+      modal: true,
+    })
+    about_window.set_application_icon('syncthing-logo-only')
+    about_window.set_application_name(_('Syncthing Toggle'))
+    about_window.set_version(`${this.metadata.version}.0`)
+    about_window.set_developer_name('rehhouari (rehhouari@gmail.com)')
+    about_window.set_issue_url(this.metadata.url + '/issues')
+    about_window.set_website(this.metadata.url)
+    about_window.set_license_type(Gtk.License.GPL_3_0)
+    about_window.set_copyright('© 2024 rehhouari')
+    about_window.show()
+  }
 }
